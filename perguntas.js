912e83f19b9c1a596eca70664445038a10c77985
Said <161731362+gulipe@users.noreply.github.com>
2024-10-21 12:17:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que e JavaScript?',
+    'O JavaScript e uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual e a capital da França?',
+    'A capital da França e Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que e uma função?',
+    'Uma função e um bloco de codigo que executa alguma tarefa'
+)
